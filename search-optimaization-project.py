1c94599ecfef17586fb9c91df191a8bb6361541f
arefehheydari <arefeh.hyd@gmail.com>
2024-12-29 16:06:40 +0330

    Add Binary Search implementation

--- search-optimaization-project.py
+++ search-optimaization-project.py
@@ -1,3 +1,18 @@
+# Binary Search
+def binary_search(data, target):
+    left, right = 0, len(data) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if data[mid] == target:
+            return mid
+        elif data[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+
+# Linear Search
 def linear_search(data, target):
     for i, item in enumerate(data):
         if item == target:
