7a1178bd61525b70cf0c844a156e7f5952ac2772
arefehheydari <arefeh.hyd@gmail.com>
2024-12-29 16:09:47 +0330

    Add Binary Search tree implementation

--- search-optimaization-project.py
+++ search-optimaization-project.py
@@ -31,6 +31,28 @@ def hash_table_search(hash_table, target):
     return hash_table.get(target, -1)
 
 
+# Binary Search Tree
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+def build_balanced_bst(data):
+    if not data:
+        return None
+    mid = len(data) // 2
+    root = TreeNode(data[mid])
+    root.left = build_balanced_bst(data[:mid])
+    root.right = build_balanced_bst(data[mid+1:])
+    return root
+
+def search_tree(root, target):
+    if root is None or root.key == target:
+        return root
+    if target < root.key:
+        return search_tree(root.left, target)
+    return search_tree(root.right, target)
 
 
 # Load data from the file
